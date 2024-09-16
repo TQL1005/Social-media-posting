f64f930b9af85d80d74c9705a15167fbe21ab7d4
TQL1005 <86201052+TQL1005@users.noreply.github.com>
2024-09-16 14:37:46 +0700

    Update settings.py

--- auto_post/auto_post/settings.py
+++ auto_post/auto_post/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-863vr#i1n^7+_lbl#*pr@pur1v#(0!e+1%+q@o4!lj@1mifxmd'
+SECRET_KEY = 'ENTER_YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
